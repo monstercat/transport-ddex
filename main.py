91c8299287070af413afa47861c2e50d759a8d87
Thomas Mathews <thomas@monstercat.com>
2017-10-13 16:29:39 -0700

    Fixed dry running.

--- main.py
+++ main.py
@@ -30,23 +30,31 @@ def send_releases (args):
                 l_xml_path = os.path.abspath(os.path.join(args.dir, path, xml_name))
                 l_rdir_path = os.path.abspath(os.path.join(args.dir, path, RESOURCES_DIR))
                 # Renames timestamp directories to the local copy.                
-                if args.update_timestamps and len(remote_map.get(grid, '')) > 0:
-                    old_filename = remote_map.get(grid)
+                old_filename = remote_map.get(grid, '')
+                if args.update_timestamps and len(old_filename) > 0 and old_filename != filename:
                     if args.verbose: print('>> Renaming %s to %s' % (old_filename, filename))
                     if not args.dry: sftp.rename(old_filename, filename)
                 if not sftp.exists(filename):
                     if args.verbose: print('>> Creating directory %s' % (filename))
                     if not args.dry: sftp.mkdir(filename)
-                with sftp.cd(filename):
+                if not args.dry: 
+                    with sftp.cd(filename):
+                        if not sftp.exists(RESOURCES_DIR):
+                            if args.verbose: print('>> Creating directory %s/%s' % (filename, RESOURCES_DIR))
+                            sftp.mkdir(RESOURCES_DIR)
+                        if not args.skip_resources:
+                            if args.verbose: print('>> Uploading resource files')
+                            sftp.put_d(l_rdir_path, RESOURCES_DIR, preserve_mtime=False)
+                        if not args.skip_existing:
+                            if args.verbose: print('>> Uploading XML file')
+                            sftp.put(l_xml_path, preserve_mtime=False)
+                elif args.verbose:
                     if not sftp.exists(RESOURCES_DIR):
-                        if args.verbose: print('>> Creating directory %s/%s' % (filename, RESOURCES_DIR))
-                        if not args.dry: sftp.mkdir(RESOURCES_DIR)
+                        print('>> Creating directory %s/%s' % (filename, RESOURCES_DIR))
                     if not args.skip_resources:
-                        if args.verbose: print('>> Uploading resource files')
-                        if not args.dry: sftp.put_d(l_rdir_path, RESOURCES_DIR, preserve_mtime=False)
+                        print('>> Uploading resource files')
                     if not args.skip_existing:
-                        if args.verbose: print('>> Uploading XML file')
-                        if not args.dry: sftp.put(l_xml_path, preserve_mtime=False)
+                        print('>> Uploading XML file')
 
 parser = argparse.ArgumentParser(description='Manages transfering DDEX files to a remote location.')
 parser.add_argument('host')
@@ -58,6 +66,6 @@ parser.add_argument('--update-timestamps', dest='update_timestamps', action='sto
 parser.add_argument('--target-dir', dest='target_dir', default='upload')
 parser.add_argument('--skip-existing', dest='skip_existing', action='store_true')
 parser.add_argument('--skip-resources', dest='skip_resources', action='store_true')
-parser.add_argument('-d', '--dry', action='store_true')
+parser.add_argument('-d', '--dry', action='store_true', help='Will not upload files, but will create the needed root directories')
 args = parser.parse_args()
 send_releases(args)
